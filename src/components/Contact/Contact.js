6e1126b39a6c4ac1bd31df756420c8d8b5203a10
iamijas14 <ijas.mi2714@gmail.com>
2022-07-20 17:59:51 +0530

    contact form feature

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,46 @@
 import './Contact.css'
-// import "https://kit.fontawesome.com/7f2b20e485.js"
+import emailjs from '@emailjs/browser';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faFacebookF,faLinkedinIn,faInstagram, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react';
 
 const Contact = () => {
+
+    const [isResult, setResult] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm(
+            'service_ut0jh4u', 
+            'template_sc8y68e', 
+            e.target, 
+            'p9Jc_xbJQAAhLguLC')
+          .then((result) => {
+              console.log(result.text);
+              console.log('message sent')
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+
+          console.log(e)
+          setResult(true)
+      };
+
+      setTimeout(() => setResult(false), 3000);
+
   return (
-    <div className='contact' id='contact'>
+    <div className='contact'>
+
+        <section id='contact'>
         <div className='contact_left'>
             <h3>Connect socially</h3>
 
             <div className="container">
 
-                    <FontAwesomeIcon className='icon-holder fab' icon= {faAngleDoubleRight} />
+                <FontAwesomeIcon className='icon-holder fab' icon= {faAngleDoubleRight} />
   
                 <ul className="sm-list">
     
@@ -50,9 +78,35 @@ const Contact = () => {
         
                 </ul>
             </div>
+        </div>
+        </section>
 
+        <div className='contact_right'>
+            <form onSubmit={sendEmail}>
+                <div className='form_name'>
+                    <label>Name</label>
+                    <input type="text" placeholder="Your Name" name="user_name" className='form_input' />
+                </div>
+
+                <div className='form_email'>
+                    <label>Email</label>
+                    <input type="email" placeholder="Your Email_Id" name="user_email" className='form_input' />
+                </div>
+                <div className='form_message'>
+                    <label>Message</label>
+                    <textarea placeholder="message" name="message" className='form_input' />
+                </div>
+
+                <div className='form_button'>
+                    {isResult ? (<p>Message sent. Thank you for your time.</p>) : null}
+                    <button type="submit" value="Send">Submit</button>
+                </div>
+            </form>
+
+            {isResult ? (<p>Message sent. Thank you for your time.</p>) : null}
         </div>
     </div>
+   
   )
 }
 
